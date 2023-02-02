8e5c71516f85b22f04e849cd94e42ad5c648c4d0
unknown <C:\Users\Computer\AppData\Roaming\The Bat!>
2023-02-02 16:01:51 +0600

    Добавить страницу поиска

---
+++ presentation/Store.Web2/Controller/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Store.Web2.Controller
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public SearchController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query)
+        {   
+            var books= bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+
+        private IActionResult View(object books)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ presetation/Store.Web/Pages/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Pages
+{
+    public class SearchController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+
+        public SearchController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var books = bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}

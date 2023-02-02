1a09f6f1857c7417ae87db55700815a9b29d97b7
unknown <C:\Users\Computer\AppData\Roaming\The Bat!>
2023-02-02 10:37:41 +0600

    Добавить Интерфейс репозитория книг

--- domain/store/Book.cs
+++ domain/store/Book.cs
@@ -2,7 +2,14 @@
 {
     public class Book
     {
+        public int Id { get; }
+        public string Title { get; }
 
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
     }
 }
 

509160c1dced1633a33a1ae51e0f21e4ef9d8d78
unknown <C:\Users\Computer\AppData\Roaming\The Bat!>
2023-02-02 12:59:04 +0600

    Добавить реализацию репозитория книг в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        new Book(1, "Art Of Programming"),
+        new Book(2, "Refactoring"),
+        new Book(3, "C Programming Language"),
+    }
+}
